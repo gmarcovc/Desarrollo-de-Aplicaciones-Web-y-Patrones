6c74747e9ccdbdb344dd473ff217b26249b2ba80
gmarcovc <gmarcovc@gmail.com>
2023-02-17 18:48:13 -0600

    Introducir HTML a JAVA
    Antes tenia el proyecto en VS code pero sin un proyecto en java, acá lo pasé a los archivos de statis de java y cree el proyecto con spring boot

---
+++ tienda/src/test/java/com/tienda_iq23/tienda/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_iq23.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
